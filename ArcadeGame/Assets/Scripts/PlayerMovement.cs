b9b66bf76dcec184550bcb0c96369610b42a4835
NieceyX <65681516+NieceyX@users.noreply.github.com>
2022-09-03 01:33:46 -0600

    Movement works

--- ArcadeGame/Assets/Scripts/PlayerMovement.cs
+++ ArcadeGame/Assets/Scripts/PlayerMovement.cs
@@ -8,15 +8,16 @@ public class PlayerMovement : MonoBehaviour
 
     float horizontal;
 
-    public float buttonTime = 0.3f;
-    public float jumpAmount = 20;
+    public float buttonTime = 0.5f;
+    public float jumpHeight = 5;
+    public float cancelRate = 100;
     float jumpTime;
     bool jumping;
+    bool jumpCancelled;
 
-    public float jumpSpeed = 8f;
+    public float runSpeed = 1.0f;
 
-    public float runSpeed = 10.0f;
-
+    public Camera camera;
 
     // Start is called before the first frame update
     void Start()
@@ -27,30 +28,39 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontal = Input.GetAxisRaw("Horizontal");
-
+        horizontal = Input.GetAxisRaw("Horizontal") + 1.1f;
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("yeah");
+            float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * body.gravityScale));
+            body.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             jumping = true;
+            jumpCancelled = false;
             jumpTime = 0;
         }
         if (jumping)
         {
-            body.velocity = new Vector2(body.velocity.x, jumpAmount);
             jumpTime += Time.deltaTime;
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                jumpCancelled = true;
+            }
+            if (jumpTime > buttonTime)
+            {
+                jumping = false;
+            }
         }
-        if (Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime)
-        {
-            jumping = false;
-        }
-
     }
-
     private void FixedUpdate()
     {
-        body.velocity = new Vector2(horizontal * runSpeed, 0);
+        this.transform.position = new Vector2(this.transform.position.x + horizontal/5, this.transform.position.y);
+        camera.transform.position = new Vector3(this.transform.position.x, camera.transform.position.y, camera.transform.position.z);
+        if (jumpCancelled && jumping && body.velocity.y > 0)
+        {
+            body.AddForce(Vector2.down * cancelRate);
+        }
     }
+
 }
+
 
 
